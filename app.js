1095c6703be6e9e5d39dfcaa5fba7d78bc9ae949
James Post <avilapost93@gmail.com>
2021-02-23 12:37:40 -0800

    final final final

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.get('/about', (request, response) => response.sendFile('/mnt/c/Users/Master/
 
 app.get('/mystery', (request, response) => response.sendFile('/mnt/c/Users/Master/Desktop/7/Express/lab-express-basic-site/views/mystery.html'));
 
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
+app.listen(process.env.PORT || 3000);
